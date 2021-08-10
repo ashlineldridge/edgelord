c97d298ea52be560646530ad2b4538c299558c40
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-08-11 06:31:41 +1000

    Cobbling basic structure together

--- worker/worker.js
+++ worker/worker.js
@@ -2,13 +2,15 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+const { greet } = wasm_bindgen;
+const instance = wasm_bindgen(wasm)
+
 /**
  * Fetch and log a request
  * @param {Request} request
  */
 async function handleRequest(request) {
-    const { greet } = wasm_bindgen;
-    await wasm_bindgen(wasm)
-    const greeting = greet()
-    return new Response(greeting, {status: 200})
+  await instance;
+  const greeting = greet()
+  return new Response(greeting, {status: 200})
 }
